4f11e4a30cfce1c631c93b1cd5c1fc857ca68282
Jefferson de Jesus Oliveira <jeffersonoliveira@kpmg.com.br>
2019-07-17 00:36:28 -0300

    First commit

---
+++ Sistema-test.Domain/Entities/Cardapio/Cardapio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_test.Domain.Entities.Cardapio
+{
+    public class Cardapio
+    {
+        public int CardapioId { get; set; }
+        public string nameProduct { get; set; }
+        public int AveragePreparationTime { get; set; }
+
+    }
+}
---
+++ Sistema-test.Domain/Entities/Cardapio/Lanches.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_test.Domain.Entities.Cardapio
+{
+    class Lanches
+    {
+        public int LanchesId { get; set; }
+        public string nameProduct { get; set; }
+        public int AveragePreparationTime { get; set; }
+        public DateTime DateCreate { get; set; }
+        public DateTime MyProperty { get; set; }
+
+    }
+}
